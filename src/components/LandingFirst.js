767512e096285d4e7000a78de5d533f43ffe8dae
Alfredo Cedeno <alfredorca@gmail.com>
2022-03-01 21:35:55 -0500

    second commit

--- src/components/LandingFirst.js
+++ src/components/LandingFirst.js
@@ -1,11 +1,23 @@
-const LadingFirst = () => {
- return (
-  <div className="welcomeBox container">
-   <p className="landingTitle">OTAKU DATING</p>
-   <p> Find someone like you. Who likes Anime.</p>
-  </div>
- 
- )
-}
+import Button from "react-bootstrap/Button";
 
-export default LadingFirst
+const LandingFirst = () => {
+  return (
+    <>
+      <div className="welcomeBox container">
+        <p className="landingTitle">OTAKU DATING</p>
+        <p> Find someone like you. Who likes Anime.</p>
+        <div className="landingButtons">
+          <Button variant="outline-success" size="lg" className="landingButton">
+            Log In
+          </Button>
+          <Button variant="outline-danger" size="lg" className="landingButton">
+            Register
+          </Button>
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingFirst;
